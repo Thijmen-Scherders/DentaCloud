12c440908bb04320c507ec3be51282cdff27d28f
MarkPrins18 <mark.prins@ziggo.nl>
2023-09-19 13:45:08 +0200

    Aparte header gemaakt en daarin connectie.php ingeladen

---
+++ header.php
@@ -0,0 +1,8 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://maxcdn.bootsrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>Dentacloud Crud</title>
+    <?php include_once("Connect"); ?>
+</head>
