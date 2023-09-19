d267b8faff0434fc88d4cf1d8b00e76d97f547f0
Thijmen-Scherders <D280394@edu.rocwb.nl>
2023-09-19 16:24:41 +0200

    Echo verwijderd

--- Connect.php
+++ Connect.php
@@ -8,14 +8,6 @@ $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password)
 
 $conn->SetAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-if ($conn)
-{
-    echo "succesfully connected";
-}
 
-else
-{
-    echo "Connection failed";
-}
 
 ?>
