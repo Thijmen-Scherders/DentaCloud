dd2998c3ef6b900e5c7b57af96a26786dd80c538
MarkPrins18 <mark.prins@ziggo.nl>
2023-09-19 13:46:01 +0200

    connect naar connectlphp veranderd

--- header.php
+++ header.php
@@ -4,5 +4,5 @@
 
     <link rel="stylesheet" href="https://maxcdn.bootsrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <title>Dentacloud Crud</title>
-    <?php include_once("Connect"); ?>
+    <?php include_once("Connect.php"); ?>
 </head>
