a572443921245c3316e3dea64aa1b9a953604b54
Thijmen-Scherders <D280394@edu.rocwb.nl>
2023-09-20 15:02:36 +0200

    Delete werkend   alleen nog push bericht

---
+++ Delete.php
@@ -0,0 +1,27 @@
+<?php
+include 'connect.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["id"])) {
+    // Ontvang de ID van het te verwijderen record
+    $recordId = $_GET["id"];
+
+    try {
+      
+        // Voer een SQL-query uit om het record uit de database te verwijderen
+        $sql = "DELETE FROM appointments WHERE Id = :recordId";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':recordId', $recordId, PDO::PARAM_INT); // Juiste variabele gebruiken
+        $stmt->execute();
+
+     
+
+        // Je kunt hier een succesmelding tonen of de gebruiker doorverwijzen naar een andere pagina
+        echo "Record $recordId is succesvol verwijderd.";
+    } catch (PDOException $e) {
+        die("Connection failed: " . $e->getMessage());
+    }
+
+
+    header("Location: index.php");
+}
+?>
