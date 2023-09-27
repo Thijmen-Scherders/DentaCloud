e7ee6908200fb1436cacb572d0caede5b898b1b8
MarkPrins18 <mark.prins@ziggo.nl>
2023-09-27 09:15:28 +0200

    javascript validatie is af

--- Create.php
+++ Create.php
@@ -20,38 +20,33 @@ logging("het afsprakenformulier bezocht", $userId);
 
 <body>
 
-
-
   <div class="container">
-    <form method="post" action="action.php">
-
+    <form name="createForm" method="post" action="action.php">
       <div class="form-group">
         <label>Voornaam</label>
-        <input type="voornaam" class="form-control" name="firstname" placeholder="Vul hier uw voornaam in">
+        <input type="text" id="firstName" class="form-control" name="firstname" placeholder="Vul hier uw voornaam in">
+
       </div>
 
       <input type="hidden" name="userId" value="<?php echo $appointment['userId']; ?>">
       <input type="hidden" name="serviceId" value="<?php echo $appointment['serviceId']; ?>">
 
-
       <div class="form-group">
         <label>Achternaam</label>
-        <input type="text" class="form-control" name="lastname" placeholder="Vul hier uw achternaam in">
+        <input type="text" class="form-control" id="lastName" name="lastname" placeholder="Vul hier uw achternaam in">
+
       </div>
-
-
 
       <div class="form-group">
         <label>Email address</label>
         <input type="email" class="form-control" name="email" id="email" placeholder="Vul hier uw email in">
-        <p id="foutmelding" style="color: red;"></p>
-      </div>
 
+      </div>
 
       <div class="form-group">
         <label>Telefoonnummer</label>
         <input type="text" class="form-control" name="phonenumber" id="telefoon" placeholder="Vul hier uw telefoonnummer in">
-        <p id="foutmelding" style="color: red;"></p>
+
       </div>
 
       <div class="form-group">
@@ -71,6 +66,7 @@ logging("het afsprakenformulier bezocht", $userId);
           }
           ?>
         </select>
+
       </div>
       <style>
         #diensten option:first-child {
@@ -78,21 +74,20 @@ logging("het afsprakenformulier bezocht", $userId);
         }
       </style>
 
-
       <div class="form-group">
         <label for="datum">Datum</label>
-        <input type="date" class="form-control" name="date" id="datum" placeholder="Vul hier uw datum in" min="<?php echo date('Y-m-d', strtotime('+0 day')); ?>">
+        <input type="date" class="form-control" name="date" id="date" placeholder="Vul hier uw datum in" min="<?php echo date('Y-m-d', strtotime('+0 day')); ?>">
+
       </div>
 
       <div class="form-group">
         <label>Tijd</label>
         <input type="time" class="form-control" id="tijd" name="time" placeholder="Vul hier uw tijd in">
-        <p id="foutmelding" style="color: red;"></p>
+
       </div>
 
-
       <input type="hidden" name="action" value="create">
-      <button type="submit" name="create" class="btn btn-primary" onclick="showSuccessPopup()">Submit</button>
+      <button type="submit" name="create" class="btn btn-primary">Submit</button>
     </form>
 
 
--- Scripts.js
+++ Scripts.js
@@ -1,59 +1,86 @@
 
-        // Eventlistener toevoegen aan het inputveld
-        document.getElementById("telefoon").addEventListener("input", function() {
-          var telefoonInput = document.getElementById("telefoon").value;
-          var telefoonRegex = /^\d{10}$/; // Dit voorbeeld accepteert 10 cijfers, pas aan volgens jouw eisen
-          var foutmeldingElement = document.getElementById("foutmelding");
+document.forms["createForm"].addEventListener("submit", function(event) {
+  var firstname = document.getElementById("firstName").value;
+  var lastname = document.getElementById("lastName").value;
+  var email = document.getElementById("email").value;
+  var phonenumber = document.getElementById("telefoon").value;
+  var serviceId = document.getElementById("diensten").value;
+  var datum = document.getElementById("date").value;
+  var tijd = document.getElementById("tijd").value;
 
-          if (telefoonInput.trim() === "") {
-            // Telefoonnummerveld is leeg, dus de foutmelding verwijderen
-            foutmeldingElement.textContent = "";
-          } else if (!telefoonRegex.test(telefoonInput)) {
-            foutmeldingElement.textContent = "Voer een geldig telefoonnummer in (bijv. 0123456789).";
-          } else {
-            foutmeldingElement.textContent = "";
-          }
-        });
+  // Clear previous error messages
+  clearErrorMessages();
 
-          // Eventlistener toevoegen aan het inputveld
-          document.getElementById("email").addEventListener("input", function() {
-          var emailInput = document.getElementById("email").value;
-          var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-          var foutmeldingElement = document.getElementById("foutmelding");
+  var errors = [];
 
-          if (emailInput.trim() === "") {
-            // E-mailveld is leeg, dus de foutmelding verwijderen
-            foutmeldingElement.textContent = "";
-          } else if (!emailRegex.test(emailInput)) {
-            foutmeldingElement.textContent = "Voer een geldig e-mailadres in.";
-          } else {
-            foutmeldingElement.textContent = "";
-          }
+  if (firstname.trim() === "") {
+    errors.push({ id: "firstName", message: "Vul uw voornaam in." });
+  }
 
+  if (lastname.trim() === "") {
+    errors.push({ id: "lastName", message: "Vul uw achternaam in." });
+  }
 
-           // Eventlistener toevoegen aan het inputveld
-        document.getElementById("tijd").addEventListener("input", function() {
-          // Huidige tijd ophalen
-          var huidigeTijd = new Date();
-          var uur = huidigeTijd.getHours();
-          var minuten = huidigeTijd.getMinutes();
+  if (email.trim() === "") {
+    errors.push({ id: "email", message: "Vul uw e-mailadres in." });
+  } else {
+    var emailInput = document.getElementById("email").value;
+    var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
-          // Ingevoerde datum en tijd ophalen uit de inputvelden
-          var ingevoerdeDatum = document.getElementById("datum").value;
-          var ingevoerdeTijd = document.getElementById("tijd").value;
-          var ingevoerdDatumTijd = new Date(ingevoerdeDatum + "T" + ingevoerdeTijd);
-
-          // Tijd vergelijken
-          if (ingevoerdDatumTijd <= huidigeTijd) {
-            document.getElementById("foutmelding").textContent = "Kies een tijd in de toekomst.";
-            document.getElementById("tijd").value = ""; // Veld leegmaken
-          } else {
-            document.getElementById("foutmelding").textContent = "";
-          }
-        });
-        });
-
-
-        function showSuccessPopup() {
-        alert('Het formulier is succesvol verzonden!');
+    if (!emailRegex.test(emailInput)) {
+      errors.push({ id: "email", message: "Voer een geldig e-mailadres in." });
     }
+  }
+
+  if (phonenumber.trim() === "") {
+    errors.push({ id: "telefoon", message: "Vul uw telefoonnummer in." });
+  } else {
+    var telefoonInput = document.getElementById("telefoon").value;
+    var telefoonRegex = /^\d{10}$/;
+
+    if (!telefoonRegex.test(telefoonInput)) {
+      errors.push({ id: "telefoon", message: "Voer een geldig telefoonnummer in (bijv. 0123456789)." });
+    }
+  }
+
+  if (serviceId.trim() === "") {
+    errors.push({ id: "diensten", message: "Selecteer een dienst." });
+  }
+
+  if (datum.trim() === "") {
+    errors.push({ id: "date", message: "Vul een datum in." });
+  }
+
+  if (tijd.trim() === "") {
+    errors.push({ id: "tijd", message: "Vul een tijd in." });
+  }
+
+  if (errors.length > 0) {
+    // Display error messages for each field, if there are any
+    for (var i = 0; i < errors.length; i++) {
+
+      // get the id of the field that contains an error, and show the message under the ID
+      var errorField = document.getElementById(errors[i].id);
+      var errorMessage = errors[i].message;
+      errorField.insertAdjacentHTML("afterend", "<p class='foutmelding' style='color: red;'>" + errorMessage + "</p>");
+    }
+    event.preventDefault(); // Prevent form to submit
+  } else {
+    showSuccessPopup();
+  }
+});
+
+function clearErrorMessages() {
+  var errorMessages = document.querySelectorAll(".foutmelding");
+  errorMessages.forEach(function(message) {
+    message.remove();
+  });
+}
+
+function showSuccessPopup() {
+  alert('Het formulier is succesvol verzonden!');
+}
+
+
+
+
