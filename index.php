14ea4ef96c9e03c78682a357945a8094139a79e4
Thijmen-Scherders <D280394@edu.rocwb.nl>
2023-09-27 09:18:02 +0200

    php naar js

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   session_start(); 
 
   require_once('header.php');
-  include 'Scripts.php';
+  include 'Scripts.js';
   
   if (isset($_SESSION['userId'])) {
       $userId = $_SESSION['userId'];
